cd47f8b45f4205e42029a75d1753acaee6609b3e
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-25 13:01:07 +0530

    by Abhaya

--- DemoGit/src/com/cjc/criatosoft/Demo.java
+++ DemoGit/src/com/cjc/criatosoft/Demo.java
@@ -1,6 +1,11 @@
 package com.cjc.criatosoft;
 
-public class Demo {
+public class Demo
+{
+	public void m1()
+	{
+		System.out.println("Hello World");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Demo Method");
