1d20312eba5b7d025c09320ff123a550e47c286d
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-25 13:07:31 +0530

    by amit

--- DemoGit/src/com/cjc/criatosoft/Demo.java
+++ DemoGit/src/com/cjc/criatosoft/Demo.java
@@ -6,6 +6,10 @@ public class Demo
 	{
 		System.out.println("Hello World");
 	}
+	public void m2()
+	{
+		System.out.println("Hello cjc");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Demo Method");
